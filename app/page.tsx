691bdcbac16e0addce9d27ba7494978e5979493e
eduardogenari <emgenari@gmail.com>
2024-07-18 12:20:38 +0200

    npm

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>kook</h1>
+      <Button>button</Button>
     </main>
+
   );
 }
